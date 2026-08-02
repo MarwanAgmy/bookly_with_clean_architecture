20abe370f70dc66972ee7c4dc67c14d695efa8d0
marwan agmy <marwanagmy53@gmail.com>
2026-08-02 22:26:55 +0300

    feat: add BookEntity class for book representation

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,13 @@
+class BookEntity {
+  final String image, title, authername;
+  final num price;
+  final num rating;
+
+  const BookEntity({
+    required this.image,
+    required this.title,
+    required this.authername,
+    required this.price,
+    required this.rating,
+  });
+}
