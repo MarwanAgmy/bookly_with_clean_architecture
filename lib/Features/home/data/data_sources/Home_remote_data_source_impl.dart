dcca1bf1d589f0810a54922ec9319bacd0db5698
marwan agmy <marwanagmy53@gmail.com>
2026-08-03 19:31:49 +0300

    feat: implement fetchFeaturedBooks method in HomeRemoteDataSourceImpl for retrieving featured books

--- lib/Features/home/data/data_sources/Home_remote_data_source_impl.dart
+++ lib/Features/home/data/data_sources/Home_remote_data_source_impl.dart
@@ -1,11 +1,28 @@
 import 'package:bookly_app/Features/home/data/data_sources/Home_remote_data_source.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl({required this.apiService});
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+      endPoint:
+          'volumes?q=general&key=AIzaSyD1Uoj4a_MoFw8aPQT6njvq5PTzAKGmSrk&filter=free-ebooks',
+    );
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
