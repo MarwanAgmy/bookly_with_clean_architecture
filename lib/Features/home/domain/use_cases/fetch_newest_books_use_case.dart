ec0a3cbd9f53ed0faa3e656cc0b2f08502af74a5
marwan agmy <marwanagmy53@gmail.com>
2026-08-03 15:37:26 +0300

    feat: add FetchNewestBooksUseCase for fetching newest books

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/no_param_use_case.dart';
+import '../entities/book_entity.dart';
+import '../repos/home_repo.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchNewestBooks();
+  }
+}
