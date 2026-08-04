d079594e672c873c9341c338f118d2cd5f70429a
marwan agmy <marwanagmy53@gmail.com>
2026-08-04 16:35:12 +0300

    feat: cache featured box

--- lib/Features/home/data/data_sources/reomte_data_source/Home_remote_data_source_impl.dart
+++ lib/Features/home/data/data_sources/reomte_data_source/Home_remote_data_source_impl.dart
@@ -1,7 +1,11 @@
 import 'package:bookly_app/Features/home/data/data_sources/reomte_data_source/Home_remote_data_source.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:hive/hive.dart';
+
+import '../../../../../core/utils/functions/save_books_data.dart';
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   final ApiService apiService;
@@ -14,6 +18,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
           'volumes?q=general&key=AIzaSyD1Uoj4a_MoFw8aPQT6njvq5PTzAKGmSrk&filter=free-ebooks',
     );
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books, kFeaturedBox);
     return books;
   }
 
