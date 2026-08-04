759701d79c9c60b30bd7620d71668595aa73703b
marwan agmy <marwanagmy53@gmail.com>
2026-08-04 16:28:04 +0300

    feat: update main function to register Hive adapter and open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,4 @@ const kPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(microseconds: 250);
 const kGTSectraFine = 'GT Sectra Fine';
 const kPaddingHeight = 15.0;
+const kFeaturedBox = 'featured_box';
