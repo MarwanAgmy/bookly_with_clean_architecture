cc1089cfcd9c7da9fc2ffd1f335409f885d16d65
marwan agmy <marwanagmy53@gmail.com>
2026-08-05 16:15:42 +0300

    feat:  crate server failure from dio error and enhance error handling in HomeRepoImpl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -24,7 +25,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(message: e.toString()));
     }
   }
 
@@ -39,7 +43,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(message: e.toString()));
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,69 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  Failure({required this.message});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.message});
+
+  factory ServerFailure.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(message: 'Connection timeout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(message: 'Send timeout with ApiServer');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(message: 'Receive timeout with ApiServer');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure(message: 'Bad certificate with ApiServer');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          dioException.response?.statusCode,
+          dioException.response?.data,
+        );
+
+      case DioExceptionType.cancel:
+        return ServerFailure(message: 'Request to ApiServer was canceled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure(message: 'No Internet Connection');
+
+      case DioExceptionType.unknown:
+        if (dioException.message != null &&
+            dioException.message!.contains('SocketException')) {
+          return ServerFailure(message: 'No Internet Connection');
+        }
+        return ServerFailure(message: 'Unexpected Error, Please try again!');
+
+      default:
+        return ServerFailure(
+          message: 'Opps There was an Error, Please try again',
+        );
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      final errorMessage =
+          response?['error']?['message'] ?? 'Authentication / Validation Error';
+      return ServerFailure(message: errorMessage);
+    } else if (statusCode == 404) {
+      return ServerFailure(
+        message: 'Your request not found, Please try later!',
+      );
+    } else if (statusCode == 500) {
+      return ServerFailure(message: 'Internal Server error, Please try later');
+    } else {
+      return ServerFailure(
+        message: 'Opps There was an Error, Please try again',
+      );
+    }
+  }
+}
