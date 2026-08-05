4e030d096285338b29756c444a07bee9a5ab94dc
marwan agmy <marwanagmy53@gmail.com>
2026-08-05 15:37:37 +0300

    feat: implement fetchNewestBooks method in HomeRepoImpl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -29,8 +29,17 @@ class HomeRepoImpl extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    List<BookEntity> books;
+    books = homeLocalDataSource.fetchNewestBooks();
+    if (books.isNotEmpty) {
+      return right(books);
+    }
+    try {
+      books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 }
