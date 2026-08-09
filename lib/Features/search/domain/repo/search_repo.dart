5b8e18ce6bc0ccc618688e0c88b51ce8cda76ab6
marwan agmy <marwanagmy53@gmail.com>
2026-08-09 14:56:56 +0300

    feat: implement SearchRepo interface for book search functionality

---
+++ lib/Features/search/domain/repo/search_repo.dart
@@ -0,0 +1,9 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class SearchRepo {
+  Future<Either<Failure, List<BookEntity>>> searchBooks({
+    required String booksName,
+  });
+}
