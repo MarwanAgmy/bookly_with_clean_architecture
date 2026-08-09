550583f58bebaff6f69ebd007487babfafe75f6a
marwan agmy <marwanagmy53@gmail.com>
2026-08-09 18:44:05 +0300

    feat: implement SearchRepoImpl with local and remote data source integration for book search

---
+++ lib/Features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,38 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/search/data/data_sources/local_data_source/search_local_data_source.dart';
+import 'package:bookly_app/Features/search/data/data_sources/remote_data_source/search_remote_data_source.dart';
+import 'package:bookly_app/Features/search/domain/repos/search_repo.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl extends SearchRepo {
+  final SearchRemoteDataSource searchRemoteDataSource;
+  final SearchLocalDataSource searchLocalDataSource;
+
+  SearchRepoImpl({
+    required this.searchRemoteDataSource,
+    required this.searchLocalDataSource,
+  });
+  @override
+  Future<Either<Failure, List<BookEntity>>> searchBooks({
+    required String bookName,
+  }) async {
+    try {
+      List<BookEntity> books = await searchRemoteDataSource
+          .searchRemoteDataSource(bookName: bookName);
+      return Right(books);
+    } catch (e) {
+      List<BookEntity> localBooks = searchLocalDataSource.searchLocalDataSource(
+        bookName: bookName,
+      );
+      if (localBooks.isNotEmpty) {
+        return Right(localBooks);
+      }
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(message: e.toString()));
+    }
+  }
+}
