42a6b10a0015fad9b8d00c0bae58bfbd33d9582f
marwan agmy <marwanagmy53@gmail.com>
2026-08-09 23:06:46 +0300

    feat: update service locator to register SearchRepo and SearchUseCase for search functionality

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,13 @@
+import 'package:bookly_app/Features/search/data/data_sources/local_data_source/search_local_data_source_impl.dart';
+import 'package:bookly_app/Features/search/data/data_sources/remote_data_source/search_remote_data_source_impl.dart';
+import 'package:bookly_app/Features/search/data/repos/search_repo_impl.dart';
+import 'package:bookly_app/Features/search/domain/repos/search_repo.dart';
+import 'package:bookly_app/Features/search/domain/use_cases/search_use_case.dart';
+import 'package:bookly_app/Features/search/presentation/manager/search_books_cubit/search_books_cubit.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/core/utils/functions/setup_service_locator.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../Features/home/domain/entities/book_entity.dart';
 import '../../Features/home/presentaion/views/home_view.dart';
@@ -20,7 +30,11 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kSearchView,
-        builder: (context, state) => const SearchView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) =>
+              SearchBooksCubit(searchUseCase: getIt.get<SearchUseCase>()),
+          child: const SearchView(),
+        ),
       ),
     ],
   );
--- lib/core/utils/functions/setup_service_locator.dart
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -1,9 +1,16 @@
 import 'package:bookly_app/Features/home/data/data_sources/local_data_source/home_local_data_source_impl.dart';
 import 'package:bookly_app/Features/home/data/data_sources/reomte_data_source/Home_remote_data_source_impl.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/Features/search/domain/repos/search_repo.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
+import '../../../Features/search/data/data_sources/local_data_source/search_local_data_source_impl.dart';
+import '../../../Features/search/data/data_sources/remote_data_source/search_remote_data_source_impl.dart';
+import '../../../Features/search/data/repos/search_repo_impl.dart';
+import '../../../Features/search/domain/use_cases/search_use_case.dart';
 
 final getIt = GetIt.instance;
 void setupServiceLocator() {
@@ -16,5 +23,17 @@ void setupServiceLocator() {
       ),
       homeLocalDataSource: HomeLocalDataSourceImpl(),
     ),
+  );
+
+  getIt.registerSingleton<SearchRepo>(
+    SearchRepoImpl(
+      searchRemoteDataSource: SearchRemoteDataSourceImpl(
+        apiService: getIt.get<ApiService>(),
+      ),
+      searchLocalDataSource: SearchLocalDataSourceImpl(),
+    ),
+  );
+  getIt.registerSingleton<SearchUseCase>(
+    SearchUseCase(searchRepo: getIt.get<SearchRepo>()),
   );
 }
